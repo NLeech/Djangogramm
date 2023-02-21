fed11b7c30754d343d9284f5dd00a4c892e835bc
Olexandr Brailovskyi <alexander.braylovskiy@gmail.com>
2023-02-21 23:54:39 +0200

    Changed gunicorn timeout to 600

--- start.sh
+++ start.sh
@@ -5,9 +5,4 @@ cd /Djangogramm/djangogramm
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py initadmin
-
-if [ "${FILL_DATABASE}" == "fill" ]; then
-  python manage.py fill_database
-fi
-
-gunicorn --bind 0.0.0.0:80 -w 1 djangogramm.wsgi
+gunicorn --timeout 600 --bind 0.0.0.0:80 -w 1 djangogramm.wsgi
